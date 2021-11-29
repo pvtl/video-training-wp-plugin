32c372adfb3603e5104db55eb02b2bd9916459ef
Matt McNamee <matt@mcnamee.co>
2021-11-29 16:08:07 +1000

    Add a spinner while loading

--- resources/assets/js/pvtl-training.js
+++ resources/assets/js/pvtl-training.js
@@ -4,4 +4,10 @@
 jQuery(document).ready(function ($) {
 	// Remove notices/messages from other plugins
 	$('.notice').hide();
+
+	// Show/Hide the iFrame
+	$('#pvtlTrainingIframe').on('load', function() {
+		$('#pvtlTrainingSpinner').hide();
+		$(this).fadeIn();
+	});
 });
--- resources/views/admin-iframe.php
+++ resources/views/admin-iframe.php
@@ -6,31 +6,39 @@
  */
 
 ?>
-<?php if ( isset( $_REQUEST['settings-updated'] ) ) : ?>
-	<div class="updated" style="margin: 0 0 15px 0;">
-		<p>Thanks, your API Key has been saved</p>
+<div class="wrap">
+	<?php if ( isset( $_REQUEST['settings-updated'] ) ) : ?>
+		<div class="updated" style="margin: 15px 0 15px 0;">
+			<p>Thanks, your API Key has been saved</p>
+		</div>
+	<?php endif; ?>
+
+
+	<div id="pvtlTrainingSpinner" style="width: 100%; display: block; float: left; padding: 200px 0; text-align: center;">
+		<div class="spinner" style="visibility: visible; float: none; margin-top: 0"></div> Loading...
 	</div>
-<?php endif; ?>
 
-<iframe
-	width="100%"
-	height="100%"
-	frameborder="0"
-	id="pvtlTrainingIframe"
-	allowtransparency="true"
-	style="overflow: hidden; min-height: calc(100vh - 32px - 65px); height: 100%; width:100%"
-	src="http://video-training.pub.localhost/training/<?php echo esc_attr( get_option( 'training_portal_slug' ) ); ?>"
-></iframe>
+	<iframe
+		width="100%"
+		height="100%"
+		frameborder="0"
+		id="pvtlTrainingIframe"
+		allowtransparency="true"
+		allowfullscreen="true"
+		style="overflow: hidden; min-height: calc(100vh - 32px - 65px); height: 100%; width:100%; display: none;"
+		src="http://video-training.pub.localhost/training/<?php echo esc_attr( get_option( 'training_portal_slug' ) ); ?>"
+	></iframe>
 
-<form action="options.php" method="post">
-	<?php
-		settings_fields( 'pvtl-training' );
-		do_settings_sections( 'pvtl-training' );
-	?>
+	<form action="options.php" method="post">
+		<?php
+			settings_fields( 'pvtl-training' );
+			do_settings_sections( 'pvtl-training' );
+		?>
 
-	<input type="hidden" name="training_portal_slug" value="" />
+		<input type="hidden" name="training_portal_slug" value="" />
 
-	<div style="float: right; margin-right: 20px;">
-		<?php submit_button( 'Reset API Key', 'small' ); ?>
-	</div>
-</form>
+		<div style="float: right; margin-right: 20px;">
+			<?php submit_button( 'Reset API Key', 'small' ); ?>
+		</div>
+	</form>
+</div>
